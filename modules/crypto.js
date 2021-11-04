d7f79b995ff08f02f06df36a32905042f8ec53b7
TristanVarewijck <tristanconnect4@outlook.com>
2021-11-04 16:16:18 +0100

    readme update

--- modules/crypto.js
+++ modules/crypto.js
@@ -2,20 +2,18 @@
 const colors = require("colors");
 const axios = require("axios").default;
 require("dotenv").config();
-
-
-
+let data = getData();
 async function getData() {
   await axios(process.env.API_URL) // respones handlers and function callers
     .then(function (res) {
       let cryptoCurrencies = res.data;
       // cryptoCurrencies wordt overschreven door de nieuwe array die is gemaakt in de functie getlastupdated
-      // 1. 
+      // 1.
       cryptoCurrencies = getLastUpdated(cryptoCurrencies);
-      //  2. 
+      //  2.
       cryptoCurrencies = getPercentage(cryptoCurrencies);
 
-      return cryptoCurrencies
+      return cryptoCurrencies;
     })
     .catch(function (err) {
       console.log(err);
@@ -25,19 +23,22 @@ async function getData() {
 function getLastUpdated(cryptoCurrencies) {
   // .map() maakt een nieuwe, lege array aan
   return cryptoCurrencies.map((currency) => {
-    currency.last_updated = currency.last_updated.slice(0, 19).replace("T", " UTC ");
+    currency.last_updated = currency.last_updated
+      .slice(0, 19)
+      .replace("T", " UTC ");
     //  return currency, omdat de nieuwe array die map aanmaakt deze ook nodig heeft
     return currency;
   });
-};
+}
 
 function getPercentage(cryptoCurrencies) {
   return cryptoCurrencies.map((currency) => {
-    currency.price_change_percentage_24h_in_currency = (Math.round(currency.price_change_percentage_24h_in_currency * 100) / 100).toString();
-    return currency
+    currency.price_change_percentage_24h_in_currency = (
+      Math.round(currency.price_change_percentage_24h_in_currency * 100) / 100
+    ).toString();
+    return currency;
   });
-};
-
+}
 
 // function aanroepen
 getData();
